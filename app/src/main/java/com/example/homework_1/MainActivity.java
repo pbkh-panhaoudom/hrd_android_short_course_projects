3386adc24c86ddc6991c91d4e9ace18a606f3c50
pbkh-panhaoudom <soum.panhaoudom@pi-pe.co.jp>
2019-10-21 16:58:25 +0700

    update homework 1

--- app/src/main/java/com/example/homework_1/MainActivity.java
+++ app/src/main/java/com/example/homework_1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.homework_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import java.util.HashMap;
@@ -12,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText email;
     private EditText password;
-    private
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +23,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Map<String, String> user = new HashMap<>();
-        user.put("user@gmail.com", "123");
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, String> user = new HashMap<>();
+                user.put("user@gmail.com", "123");
+                email = findViewById(R.id.EditText_Email);
+                password = findViewById(R.id.EditText_Password);
 
+                if (user.get(email.getText().toString()) != null &&
+                    user.get(email.getText().toString()).equals(password.getText().toString())){
+                    Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
 
     }
 }
